df763cb8848a07e99c2e323aee24b1e40e424ea0
presly808 <presly808@gmail.com>
2017-12-16 11:49:44 +0200

    fixed Integration

--- src/main/java/ua/artcode/todo/integrational_tests/RunIntegrationTest.java
+++ src/main/java/ua/artcode/todo/integrational_tests/RunIntegrationTest.java
@@ -23,7 +23,7 @@ public class RunIntegrationTest {
     public void testAddTodo() throws UnirestException {
         Todo forSend = new Todo(1, "testTitle", "testBody", false);
 
-        HttpResponse<Todo> response = Unirest.post("http://localhost:8080/add-todo")
+        HttpResponse<Todo> response = Unirest.post("http://localhost:5000/add-todo")
                 .header("accept", "application/json")
                 .header("Content-Type", "application/json")
                 .body(forSend)
@@ -32,7 +32,7 @@ public class RunIntegrationTest {
         Todo todoReturned = response.getBody();
 
         Assert.assertEquals(201, response.getStatus());
-        Assert.assertEquals(forSend.getId(),todoReturned.getId());
+        Assert.assertEquals(forSend.getTitle(),todoReturned.getTitle());
     }
 
 
